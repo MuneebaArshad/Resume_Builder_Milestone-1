a3e9f0650d4b3793e5fe5830f0962b2cdd3857a3
Muneeba Arshad <161239150+MuneebaArshad@users.noreply.github.com>
2024-11-10 02:53:37 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+// -----------skills
+var skills_section = document.getElementById('skil');
+var skill_btn = document.getElementById('skill_btn');
+skill_btn === null || skill_btn === void 0 ? void 0 : skill_btn.addEventListener('click', function () {
+    skills_section.classList.toggle('hide');
+});
+// -----------Experience
+var exp_section = document.getElementById('expe');
+var exp_btn = document.getElementById('Experience_btn');
+exp_btn === null || exp_btn === void 0 ? void 0 : exp_btn.addEventListener('click', function () {
+    exp_section.classList.toggle('hide');
+});
+// -----------profile
+var Profile_section = document.getElementById('prof');
+var Profile_btn = document.getElementById('Profile_btn');
+Profile_btn === null || Profile_btn === void 0 ? void 0 : Profile_btn.addEventListener('click', function () {
+    Profile_section.classList.toggle('hide');
+});
+// -----------language
+var lang_section = document.getElementById('lang');
+var lang_btn = document.getElementById('lang_btn');
+lang_btn === null || lang_btn === void 0 ? void 0 : lang_btn.addEventListener('click', function () {
+    lang_section.classList.toggle('hide');
+});
+// -----------Education
+var edu_section = document.getElementById('educ');
+var educ_btn = document.getElementById('Educ_btn');
+educ_btn === null || educ_btn === void 0 ? void 0 : educ_btn.addEventListener('click', function () {
+    edu_section.classList.toggle('hide');
+});
+// -----------profile
+var cont_section = document.getElementById('cont');
+var contact_btn = document.getElementById('contact_btn');
+contact_btn === null || contact_btn === void 0 ? void 0 : contact_btn.addEventListener('click', function () {
+    cont_section.classList.toggle('hide');
+});
---
+++ app.ts
@@ -0,0 +1,56 @@
+// -----------skills
+let skills_section = document.getElementById('skil')
+let skill_btn = document.getElementById('skill_btn')
+
+
+
+skill_btn?.addEventListener('click', ()=>{
+    skills_section!.classList.toggle('hide')
+})
+
+
+// -----------Experience
+let exp_section =document.getElementById('expe')
+let exp_btn =document.getElementById('Experience_btn')
+
+exp_btn?.addEventListener('click', ()=>{
+    exp_section!.classList.toggle('hide')
+})
+
+
+// -----------profile
+let Profile_section =document.getElementById('prof')
+let Profile_btn=document.getElementById('Profile_btn')
+
+Profile_btn?.addEventListener('click', ()=>{
+    Profile_section!.classList.toggle('hide')
+})
+
+
+// -----------language
+let lang_section =document.getElementById('lang')
+let lang_btn=document.getElementById('lang_btn')
+
+lang_btn?.addEventListener('click', ()=>{
+    lang_section!.classList.toggle('hide')
+})
+
+
+// -----------Education
+let edu_section =document.getElementById('educ')
+let educ_btn=document.getElementById('Educ_btn')
+
+educ_btn?.addEventListener('click', ()=>{
+    edu_section!.classList.toggle('hide')
+})
+
+
+// -----------profile
+let cont_section =document.getElementById('cont')
+let contact_btn=document.getElementById('contact_btn')
+
+contact_btn?.addEventListener('click', ()=>{
+    cont_section!.classList.toggle('hide')
+})
+
+ 
